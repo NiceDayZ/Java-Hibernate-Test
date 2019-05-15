c256a317049cad311fefb980c44c2fb4f93620e3
NiceDayZ <craciunmihai40@gmail.com>
2019-05-15 22:00:48 +0300

    just a test

---
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -0,0 +1,37 @@
+package com.teamtreehouse.contactmgr;
+
+import com.teamtreehouse.contactmgr.model.Contact;
+import com.teamtreehouse.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class Application {
+    // Hold a reusable reference to a SessionFactory (since we need only one)
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        Contact contact = new ContactBuilder("Chris","Ramacciotti")
+                .withEmail("rama@teamtreehouse.com")
+                .withPhone(7735556666L)
+                .build();
+
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+        session.save(contact);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
